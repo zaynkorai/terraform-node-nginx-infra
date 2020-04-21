c94a3f7c74093d4e330caf4657fa124accd04e34
Zayn-ul-Abdin <zaynulabdin313@gmail.com>
2020-04-21 15:28:05 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 # # Creds and default location
-variable "credentials" { default = "keys/service-account.json" } // Change with you service account .json file
+variable "credentials" { default = "auth/service-account.json" } // Change with you service account .json file
 variable "project" { default = "" }                              // Your GCP Project ID
 variable "region" { default = "us-central1" }
 variable "zone" { default = "us-central1-a" }
